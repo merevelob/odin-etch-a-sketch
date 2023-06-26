64b83aefabf72c47d06efea58e6095699270e494
Mauricio Revelo <122694874+merevelob@users.noreply.github.com>
2023-06-25 21:35:21 -0500

    Add reset button

--- script.js
+++ script.js
@@ -20,11 +20,11 @@ createGrid(16);
 
 // Change squares per side and display selection in the sidebar
 function changeSquares() {
-    let input = +prompt('Please enter number of squares per side.', '');
+    let input = +prompt('Please enter number of squares per side.', '1');
     container.innerHTML = '';
     if (input < 1) {
         alert('1 is the min number of squares per side.');
-        input = 1;
+        input = 16;
     }
     if (input > 100) {
         alert('100 is the max number of squares per side.');
@@ -37,3 +37,13 @@ function changeSquares() {
 const btn = document.querySelector('.squaresNumber > button');
 const sqDisplay = document.querySelector('.squaresNumber > p');
 btn.addEventListener('click', changeSquares);
+
+// Reset grid
+function resetGrid() {
+    container.innerHTML = '';
+    sqDisplay.textContent = `16 x 16`;
+    createGrid(16);
+}
+
+const resetBtn = document.querySelector('.btn-reset');
+resetBtn.addEventListener('click', resetGrid);
